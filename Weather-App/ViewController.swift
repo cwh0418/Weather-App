8b9c64a4d3be7c5156c834fd7c1807a88998a06f
cwh0418 <Woonghee@jounghuiui-macbook-pro.local>
2016-02-16 17:22:11 +0900

    Initial Commit

---
+++ Weather-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather-App
+//
+//  Created by 조웅희 on 2016. 2. 16..
+//  Copyright © 2016년 Sweet Acorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
